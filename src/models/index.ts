e15b67523be1316d71daa91cc12d3922d1a9d358
Rune <212063551@qq.com>
2023-10-09 23:49:54 +0800

    add: 新增文章表

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,1 +1,4 @@
 export { default as Users } from './users';
+export { default as Articles } from './article';
+export { default as Tags } from './article/tag';
+export { default as Categorys } from './article/category';
